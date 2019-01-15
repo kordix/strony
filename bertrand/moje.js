c325c70669550b462acef8949c2013e8cde23afd
= <=>
2019-01-15 22:16:30 +0100

    bertrand

---
+++ bertrand/moje.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+$('.dropdown').click(function(){
+    $('.dropdown-menu').css('opacity','1');
+    $('.dropdown-item').css('opacity','1');
+});
+
+
+});
+
+//
+//
+//
+// $('.dropdown).on('show.bs.dropdown', function () {
+//     console.log('fdsa');
+//     $('.dropdown-menu').css('opacity','0.5');
+//     $('.dropdown-item').css('opacity','0.5');
+// })
