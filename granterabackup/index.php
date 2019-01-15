1b9a6df87906390f8a9ce7e1739857501e737984
Kordian KB. Bober <kbober@wiked.pl>
2019-01-15 19:37:31 +0100

    grantera navbar topsection

---
+++ granterabackup/index.php
@@ -0,0 +1,87 @@
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Grantera kopia</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+  </head>
+  <body>
+
+
+<?php include 'views/navbar.html' ?>
+
+<div class="container-fluid">
+  <div class="row">
+
+  <div class="col-md-3 topdiv topsection">
+    <img class="topsectionimg" src="https://g7.gazetaprawna.pl/p/_wspolne/pliki/2959000/2959339-biznes-firma.jpg" alt="">
+    <div class="row">
+      <div class="mx-auto topdiv">
+        <a class="topicon" href="#"><i class="fas fa-brain"></i></i></a>
+      </div>
+    </div>
+    <div class="row">
+      <div class="mx-auto topdiv">
+        <p><a class="toptext" href="#">Tworzenie i weryfikacja pomysłów na innowacje</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-3 topdiv topsection">
+    <img class="topsectionimg" src="https://g1.gazetaprawna.pl/p/_wspolne/pliki/3222000/3222594-spolka-firma-657-323.jpg" alt="">
+    <div class="row">
+      <div class="mx-auto topdiv">
+        <a class="topicon" href="#"><i class="fas fa-cogs"></i></a>
+      </div>
+    </div>
+    <div class="row">
+      <div class="mx-auto topdiv">
+        <p><a class="toptext" href="#">Organizacja B+R</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-3 topdiv topsection">
+    <img class="topsectionimg" src="http://praca.plportal.pl/wp-content/uploads/2017/02/12.jpg" alt="">
+
+    <div class="row">
+      <div class="mx-auto topdiv">
+        <a class="topicon" href="#"> <svg class="mysvg" viewBox="0 0 100 100"><use xlink:href="chart.svg#Capa_1"></use></svg> </a>
+        <!-- <img class="img-fluid mysvg "  src="assets/chart.svg" alt="chart">  -->
+      </div>
+    </div>
+    <div class="row">
+      <div class="mx-auto topdiv">
+        <p> <a class="toptext" href="#">Wprowadzanie na rynek / aktywizacja sprzedaży </a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-3 topdiv topsection">
+    <img class="topsectionimg" src="http://www.mragowo.pl/images/o/phocagallery/dg/biznes.jpg" alt="">
+
+    <div class="row">
+      <div class="mx-auto topdiv">
+        <a class="topicon" href="#"><i class="fas fa-hand-holding-usd"></i></a>
+      </div>
+    </div>
+    <div class="row">
+      <div class="mx-auto topdiv">
+        <p><a class="toptext" href="#">Zdobywanie funduszy</a></p>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/popper.min.js" type="text/javascript"></script>
+
+<script src="js/bootstrap.js" type="text/javascript"></script>
+  </body>
+</html>
