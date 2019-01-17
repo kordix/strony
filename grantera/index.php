39842123760f77e57f029680fa77065a38ab0662
Kordian <kordian.bober@wp.pl>
2019-01-17 16:06:01 +0100

    slide

--- grantera/index.php
+++ grantera/index.php
@@ -1,87 +1,67 @@
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Grantera kopia</title>
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-  </head>
-  <body>
 
+<head>
+  <meta charset="utf-8">
+  <title>Grantera kopia</title>
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/top2.css">
 
-<?php include 'views/navbar.html' ?>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+</head>
 
-<div class="container-fluid">
-  <div class="row">
+<body>
 
-  <div class="col-md-3 topdiv topsection">
-    <img class="topsectionimg" src="https://g7.gazetaprawna.pl/p/_wspolne/pliki/2959000/2959339-biznes-firma.jpg" alt="">
+<?= $slides[0] ?>
+  <?php include 'views/navbar.html' ?>
+  <?php include 'views/topsection1.html' ?>
+
+  <div class="container-fluid topsection2">
     <div class="row">
-      <div class="mx-auto topdiv">
-        <a class="topicon" href="#"><i class="fas fa-brain"></i></i></a>
+      <div class="col-md-4 tp2-1">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <p>slide 1</p>
+              <p>slide 1</p>
+              <p>slide 1</p>
+            </div>
+            <div class="carousel-item">
+              <p>slide 2</p>
+
+            </div>
+            <div class="carousel-item">
+              <p>slide 3</p>
+
+            </div>
+          </div>
+
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+      <div class="col-md-4">
+        asdf
+      </div>
+      <div class="col-md-4">
+        asdf
       </div>
     </div>
-    <div class="row">
-      <div class="mx-auto topdiv">
-        <p><a class="toptext" href="#">Tworzenie i weryfikacja pomysłów na innowacje</a></p>
-      </div>
-    </div>
+
+
   </div>
 
-  <div class="col-md-3 topdiv topsection">
-    <img class="topsectionimg" src="https://g1.gazetaprawna.pl/p/_wspolne/pliki/3222000/3222594-spolka-firma-657-323.jpg" alt="">
-    <div class="row">
-      <div class="mx-auto topdiv">
-        <a class="topicon" href="#"><i class="fas fa-cogs"></i></a>
-      </div>
-    </div>
-    <div class="row">
-      <div class="mx-auto topdiv">
-        <p><a class="toptext" href="#">Organizacja B+R</a></p>
-      </div>
-    </div>
-  </div>
 
-  <div class="col-md-3 topdiv topsection">
-    <img class="topsectionimg" src="http://praca.plportal.pl/wp-content/uploads/2017/02/12.jpg" alt="">
-
-    <div class="row">
-      <div class="mx-auto topdiv">
-        <a class="topicon" href="#"> <svg class="mysvg" viewBox="0 0 100 100"><use xlink:href="chart.svg#Capa_1"></use></svg> </a>
-        <!-- <img class="img-fluid mysvg "  src="assets/chart.svg" alt="chart">  -->
-      </div>
-    </div>
-    <div class="row">
-      <div class="mx-auto topdiv">
-        <p> <a class="toptext" href="#">Wprowadzanie na rynek / aktywizacja sprzedaży </a></p>
-      </div>
-    </div>
-  </div>
-
-  <div class="col-md-3 topdiv topsection">
-    <img class="topsectionimg" src="http://www.mragowo.pl/images/o/phocagallery/dg/biznes.jpg" alt="">
-
-    <div class="row">
-      <div class="mx-auto topdiv">
-        <a class="topicon" href="#"><i class="fas fa-hand-holding-usd"></i></a>
-      </div>
-    </div>
-    <div class="row">
-      <div class="mx-auto topdiv">
-        <p><a class="toptext" href="#">Zdobywanie funduszy</a></p>
-      </div>
-    </div>
-  </div>
-
-</div>
-
-
-</div>
-
-
-<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/popper.min.js" type="text/javascript"></script>
 
-<script src="js/bootstrap.js" type="text/javascript"></script>
-  </body>
+  <script src="js/bootstrap.js" type="text/javascript"></script>
+</body>
+
 </html>
---
+++ grantera/resources/slides.php
@@ -0,0 +1,7 @@
+<?php
+$slides = Array();
+$slides[0]->text = 'asdffsda';
+$slides[0]->caption='caption fasd';
+print_r($slides);
+
+ ?>
