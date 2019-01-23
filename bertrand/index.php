235f796f6b529764fd4dbd3fb34d6719d54a778d
Kordian KB. Bober <kbober@wiked.pl>
2019-01-23 20:38:39 +0100

    carousel mid

--- bertrand/index.php
+++ bertrand/index.php
@@ -8,44 +8,38 @@
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="css/navbar.css">
   <link rel="stylesheet" href="css/carousel.css">
+  <link rel="stylesheet" href="css/effects.css">
+  <link rel="stylesheet" href="css/mid.css">
+
+
 
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
   <title>index.html</title>
 </head>
 
 <body>
-<?php include 'views/navbar.html' ?>
+  <?php include 'views/navbar.html' ?>
+  <?php include 'views/carousel.html' ?>
 
-<div id="carouseltop" class="carousel slide" data-ride="carousel" data-interval="20000">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active" id="slide1">
-      <div class="slidebg1">
-        <p class="carousel-title">Okna BB line </p>
+  <section id="mid" class="container-fluid">
+    <div class="container">
+
+      <div class="col-md-4" style="display:flex;align-items:center;height:400px">
+        <div>
+          <h3>Oferta XXX</h3>
+          <p> Produkty XXX charakteryzują się najwyższą jakością, potwierdzoną certyfikatami. Do produkcji stolarki firma stosuje nowoczesne technologie oraz wykorzystuje szeroką gamę materiałów: różne gatunki drewna, systemy profili PVC i
+            aluminiowych, a także materiały drewniano-aluminiowe i PVC-aluminiowe</p>
+        </div>
 
       </div>
-      <!-- <img class="img-fluid" src="images/slide1.png" alt="First slide"> -->
+      <div class="col-md-8">
+        div
+      </div>
     </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Second slide">
     </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouseltop" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouseltop" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
+  </section>
+
+
 
 
 
