b2f4ffcdf97d7096d46142cf5e2f9093178a080d
Kordian KB. Bober <kbober@wiked.pl>
2019-01-25 19:36:16 +0100

    realmid

--- bertrand/index.php
+++ bertrand/index.php
@@ -9,7 +9,7 @@
   <link rel="stylesheet" href="css/navbar.css">
   <link rel="stylesheet" href="css/carousel.css">
   <link rel="stylesheet" href="css/effects.css">
-  <link rel="stylesheet" href="css/mid.css">
+  <link rel="stylesheet" href="css/topmid.css">
   <link rel="stylesheet" href="css/style.css">
 
 
@@ -22,9 +22,9 @@
 <body>
   <?php include 'views/navbar.html' ?>
   <?php include 'views/carousel.html' ?>
-  <?php include 'views/mid.html' ?>
+  <?php include 'views/top.html' ?>
 
-<section class="container" id="mid2">
+<section class="container" id="top2">
 <h1><i class="h-initial">G</i>dzie <b>kupić</b></h1>
 <p>Nasze salony i partnerzy służą Państwu fachowym doradztwem i pomocą. To miejsca przygotowane specjalnie dla Państwa, by dostarczyć informacji oraz dopasować najlepszy produkt w pełni spełniający Państwa potrzeby.</p>
 <div class="row">
@@ -37,6 +37,37 @@
 </div>
 </section>
 
+
+<section id="mid1" class="container">
+<h1><i class="h-initial">K</i>Konfigurator drzwi premium</h1>
+<div class="row">
+  <div class="col-md-6" id="m1">
+    <div class="row">
+
+    <div class="col-md-1 d-flex align-items-center" style="padding:10px">
+
+    <i class="fas fa-window-restore" id="wicon"></i>
+  </div>
+    <div class="col-md-10">
+
+    <h3>STWÓRZ SWOJE DRZWI MARZEŃ</h3>
+    <p>Wybierz rodzaj fasady, wypełnienia, zmieniaj kolory, pochwyty, użyj szkła lub nie. Twoja decyzja, Twoje drzwi - natychmiastowy efekt.</p>
+  </div>
+
+  </div>
+</div>
+
+  <div class="col-md-6">
+
+  </div>
+
+</div>
+
+<div class="row">
+
+</div>
+
+</section>
 
 
 
